9b1ecfdd99ee6fb1cc6b5c14a2072ae520257b17
MasterSon05 <ali.ustaoglu@icloud.com>
2020-01-21 14:20:59 +0300

    guncelleme

--- lib/notification_handler.dart
+++ lib/notification_handler.dart
@@ -115,10 +115,11 @@ class NotificationHandler {
       Navigator.of(myContext, rootNavigator: true).push(
         MaterialPageRoute(
           builder: (context) => ChangeNotifierProvider(
-            builder: (context) => ChatViewModel(
+            create: (context) => ChatViewModel(
                 currentUser: _userModel.user,
                 sohbetEdilenUser: User.idveResim(
                     userID: gelenBildirim["data"]["gonderenUserID"],
+                    userName: gelenBildirim["data"]["title"],
                     profilURL: gelenBildirim["data"]["profilURL"])),
             child: SohbetPage(),
           ),
